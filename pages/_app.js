42a4a680f5878a7e84649001f1d9a50b8b31f910
osgsm <50955473+oshimax@users.noreply.github.com>
2023-01-17 18:11:31 +0900

    Support GoogleAnalytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { GA_TRACKING_ID, pageview } from '../lib/gtag';
+import GoogleAnalytics from '../components/googleanalytics';
 import '../styles/global.css';
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      {GA_TRACKING_ID && <GoogleAnalytics />}
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default App;
