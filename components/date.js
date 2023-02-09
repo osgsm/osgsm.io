bd94fa854100002bf7fea48a8cc098114ab06825
osgsm <50955473+oshimax@users.noreply.github.com>
2023-02-09 16:43:34 +0900

    Modify Date

--- components/date.js
+++ components/date.js
@@ -1,8 +1,10 @@
 import { parseISO, format } from 'date-fns';
 
-const Date = ({ dateString }) => {
+const Date = ({ dateString, hideDays }) => {
   const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
+  const formatString = hideDays ? 'LLLL, yyyy' : 'LLLL d, yyyy';
+
+  return <time dateTime={dateString}>{format(date, formatString)}</time>;
 };
 
 export default Date;
