a4e34b675017d25abfcff3de63e3a1ceb8aaf633
osgsm <50955473+oshimax@users.noreply.github.com>
2023-02-11 13:15:54 +0900

    Add links

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import { AiFillGithub, AiOutlineTwitter } from 'react-icons/ai';
+import { HiArrowUpRight } from 'react-icons/hi2';
 import Layout, { siteTitle } from '../components/layout';
 import Timeline from '../components/timeline';
 import timelineContents from '../content/aboutTimelineContents';
@@ -19,6 +21,38 @@ const About = () => {
         <p>
           大島翔吾と申します。ウェブ制作をしています。モットーは、使いやすく魅力的なサイトを作ること。コーディングだけでなく、デザイン、写真撮影もやります。
         </p>
+        <div className="mb-10 flex gap-4">
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://twitter.com/osgsm_"
+            className="flex w-full items-center justify-between
+              rounded-md border border-gray-200 bg-gray-100
+              p-3 text-sm text-gray-700
+              no-underline hover:bg-gray-200 hover:text-gray-800"
+          >
+            <div className="flex items-center">
+              <AiOutlineTwitter />
+              <span className="ml-1">Twitter</span>
+            </div>
+            <HiArrowUpRight />
+          </a>
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://github.com/osgsm"
+            className="flex w-full items-center justify-between
+              rounded-md border border-gray-200 bg-gray-100
+              p-3 text-sm text-gray-700
+              no-underline hover:bg-gray-200 hover:text-gray-800"
+          >
+            <div className="flex items-center">
+              <AiFillGithub />
+              <span className="ml-1">GitHub</span>
+            </div>
+            <HiArrowUpRight />
+          </a>
+        </div>
         <p>
           以下は自身の半生で、思考や行動の変遷を辿っています。輝かしい経歴はないですが、興味のある方は読んでみてください。
         </p>
