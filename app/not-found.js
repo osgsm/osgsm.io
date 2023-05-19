442f198f991225eeced16d7f948b9587f09ade62
osgsm <50955473+oshimax@users.noreply.github.com>
2023-05-19 12:32:20 +0900

    Add styles for 404

--- app/not-found.js
+++ app/not-found.js
@@ -3,9 +3,18 @@ import Link from 'next/link';
 const NotFound = () => {
   return (
     <>
-      <h1>Not Found</h1>
-      <p>リクエストされたページが見つかりません</p>
-      <Link href="/">ホームにもどる</Link>
+      <h1 className="mt-12 text-3xl">Not Found</h1>
+      <p className="mt-4">リクエストされたページが見つかりません</p>
+      <div className="mt-6">
+        <Link
+          className="inline-block rounded-md border border-gray-300
+            bg-gray-100 p-3 text-sm text-gray-700
+            hover:border-gray-300 hover:bg-gray-200 hover:text-gray-800"
+          href="/"
+        >
+          ホームにもどる
+        </Link>
+      </div>
     </>
   );
 };
