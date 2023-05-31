378e0785e455281c286ef612e1aea5fb7befc5da
osgsm <50955473+oshimax@users.noreply.github.com>
2023-05-31 15:45:53 +0900

    Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,37 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        background: 'var(--background)',
+        border: 'var(--border)',
+        ring: 'var(--ring)',
+        'misty-slate': {
+          50: '#F9F9FB',
+          100: '#EDEDF2',
+          200: '#E2E2E9',
+          300: '#C4C4D4',
+          400: '#A7A7BE',
+          500: '#8A8AA8',
+          600: '#6C6C93',
+          700: '#575775',
+          800: '#414158',
+          900: '#2B2B3B',
+          950: '#16161D',
+        },
+        'twilight-indigo': {
+          50: '#F5F5FE',
+          100: '#E3E3FD',
+          200: '#D1D1FA',
+          300: '#A8A8F0',
+          400: '#8585E0',
+          500: '#6666CC',
+          600: '#4D4DB2',
+          700: '#42428A',
+          800: '#323267',
+          900: '#2D2D53',
+          950: '#1F1F2E',
+        },
+      },
       typography: (theme) => ({
         DEFAULT: {
           css: {
