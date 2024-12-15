869427ca342803d711b15eb0eb9385f58560fa93
osgsm <50955473+osgsm@users.noreply.github.com>
2024-12-15 15:56:39 +0900

    Fix icon and og image

--- app/icon.tsx
+++ app/icon.tsx
@@ -1,4 +1,4 @@
-import { gray } from "@radix-ui/colors";
+import { iris } from "@radix-ui/colors";
 import { ImageResponse } from "next/og";
 
 export const size = {
@@ -9,30 +9,32 @@ export const contentType = "image/png";
 
 export default function Icon() {
   return new ImageResponse(
-    <div
-      style={{
-        background: gray.gray8,
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        borderRadius: 100,
-        padding: 4,
-      }}
-    >
+    (
       <div
         style={{
-          background: gray.gray12,
+          background: iris.iris11,
           width: "100%",
           height: "100%",
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
           borderRadius: 100,
+          padding: 4,
         }}
-      />
-    </div>,
+      >
+        <div
+          style={{
+            background: iris.iris12,
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: 100,
+          }}
+        />
+      </div>
+    ),
     {
       ...size,
     },
