858adcaf6520a777e82ddbb7244eda13c0ca2ceb
osgsm <50955473+osgsm@users.noreply.github.com>
2024-12-15 14:10:10 +0900

    Improve styles

--- components/posts/index.tsx
+++ components/posts/index.tsx
@@ -10,7 +10,9 @@ interface PostProps {
 
 export const Posts = ({ category }: PostProps) => {
   const posts = getPosts(category).sort((a, b) => {
-    return new Date(b.time.created).getTime() - new Date(a.time.created).getTime();
+    return (
+      new Date(b.time.created).getTime() - new Date(a.time.created).getTime()
+    );
   });
 
   const Seperator = () => <div className="border-border border-t" />;
@@ -20,8 +22,11 @@ export const Posts = ({ category }: PostProps) => {
   }
 
   return (
-    <div className="mt-8 flex flex-col">
-      <NextViewTransition href={`/${category}`} className="flex justify-between">
+    <div className="mt-16 flex flex-col">
+      <NextViewTransition
+        href={`/${category}`}
+        className="flex justify-between"
+      >
         <h2 className="border-0 py-2 text-muted text-xl capitalize">
           {category} {posts.length > 0 && `(${posts.length})`}
         </h2>
@@ -31,9 +36,14 @@ export const Posts = ({ category }: PostProps) => {
         return (
           <React.Fragment key={post.slug}>
             <Seperator />
-            <NextViewTransition href={`/${category}/${post.slug}`} className="flex w-full flex-col justify-between gap-0 py-2 md:flex-row md:gap-8">
+            <NextViewTransition
+              href={`/${category}/${post.slug}`}
+              className="flex w-full flex-col justify-between gap-0 py-2 md:flex-row md:gap-8"
+            >
               <p>{post.title}</p>
-              <p className="mt-0 shrink-0 text-muted">{formatter.date(new Date(post.time.created))}</p>
+              <p className="mt-0 shrink-0 text-muted">
+                {formatter.date(new Date(post.time.created))}
+              </p>
             </NextViewTransition>
           </React.Fragment>
         );
--- components/screens/home/index.tsx
+++ components/screens/home/index.tsx
@@ -7,27 +7,11 @@ export default function Home() {
   return (
     <FadeIn.Container>
       <FadeIn.Item>
-        <div className="flex justify-between text-muted">
-          <div>
-            <h1 className="animate-move-background bg-[length:600%] bg-gradient-to-r from-60% from-[--iris-10] via-70% via-[--plum-10] to-80% to-[--iris-11] bg-clip-text text-2xl text-transparent">
-              osgsm.io
-            </h1>
-          </div>
-        </div>
-      </FadeIn.Item>
-      <Spacer />
-      <FadeIn.Item>
-        <div className="grid gap-1 *:mt-0">
-          <p>ようこそ👋🏼</p>
-          <p>
-            おおしましょうご（
-            <a className="text-muted underline" href="https://bsky.app/profile/osgsm.io" target="_blank" rel="noopener noreferrer">
-              @osgsm.io
-            </a>
-            ）です。
-          </p>
-          <p>私は大阪在住のウェブデベロッパー。</p>
-          <p>このサイトは、制作のための遊び場です。</p>
+        <div className="grid gap-1 text-2xl *:mt-0">
+          <p>ようこそ👋🏼 おおしましょうごです。</p>
+          <p>大阪在住のウェブデベロッパーです。</p>
+          <p>ここは制作のための遊び場です。</p>
+          <p>作ったものや学んだこと、試したことを共有します。</p>
         </div>
       </FadeIn.Item>
       <FadeIn.Item>
