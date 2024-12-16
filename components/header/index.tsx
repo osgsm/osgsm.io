09174013ddb9741cfcb7299f8da0f419d6bbc5dc
osgsm <50955473+osgsm@users.noreply.github.com>
2024-12-16 22:33:18 +0900

    Fix logo style

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -5,10 +5,10 @@ const Header = () => {
     <header className="mx-auto flex w-full max-w-4xl items-center justify-between p-4 md:p-6">
       <Link className="transition" href="/">
         <div
-          className="h-[20px] w-[70px] animate-move-background bg-[length:600%] bg-gradient-to-r from-60% from-[--iris-10] via-70% via-[--plum-10] to-80% to-[--iris-11]"
+          className="size-[32px] animate-move-background bg-[length:600%] bg-gradient-to-br from-60% from-[--iris-10] via-75% via-[--plum-10] to-90% to-[--cyan-11]"
           style={{
-            maskImage: "url(/assets/images/logo-horizontal.svg)",
-            WebkitMaskImage: "url(/assets/images/logo-horizontal.svg)",
+            maskImage: "url(/assets/images/logo-square.svg)",
+            WebkitMaskImage: "url(/assets/images/logo-square.svg)",
             maskPosition: "center",
             WebkitMaskPosition: "center",
             maskSize: "contain",
