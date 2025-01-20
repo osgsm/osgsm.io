7bf3783f61f81ffea55a88185fd7d33dcb785dcc
osgsm <50955473+osgsm@users.noreply.github.com>
2025-01-21 00:32:52 +0900

    Update post component

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="w:full sticky top-0 z-50 my-6 grid place-items-center bg-gradient-to-b from-5% from-[--iris-1] to-50% px-4 py-2">
+    <header className="w:full sticky top-0 z-50 grid place-items-center bg-gradient-to-b from-5% from-[--iris-1] to-50% px-4 py-3 lg:my-6">
       <div className="gradient-card flex w-full items-center justify-between gap-4 rounded-xl border border-iris-4 px-4 py-2 backdrop-blur-md [--gradient-from:var(--iris-a4)] [--gradient-to:var(--iris-a3)] [--shadow-accent-color:var(--iris-a4)] [--shadow-base-color:var(--iris-a2)] lg:w-fit dark:border-0 dark:backdrop-brightness-[0.2]">
         <Link className="transition" href="/">
           <div
-            className="size-[32px] animate-move-background bg-[length:600%] bg-gradient-to-br from-50% from-[--iris-8] via-[--iris-9] to-90% to-[--iris-10]"
+            className="size-7 animate-move-background bg-[length:600%] bg-gradient-to-br from-50% from-[--iris-8] via-[--iris-9] to-90% to-[--iris-10]"
             style={{
               maskImage: "url(/assets/images/logo.svg)",
               WebkitMaskImage: "url(/assets/images/logo.svg)",
@@ -20,7 +20,7 @@ const Header = () => {
           />
           <div className="sr-only">osgsm.io</div>
         </Link>
-        <div className="flex items-center gap-2">
+        <div className="flex items-center">
           {[
             { label: "Home", href: "/" },
             { label: "Blog", href: "/blog" },
--- components/posts/index.tsx
+++ components/posts/index.tsx
@@ -22,32 +22,35 @@ export const Posts = ({ category }: PostProps) => {
   }
 
   return (
-    <div className="mt-16 flex flex-col px-4 md:px-6">
+    <div className="mt-16 grid grid-cols-12 gap-8 pb-10">
       <NextViewTransition
         href={`/${category}`}
-        className="flex justify-between no-underline"
+        className="col-span-full flex justify-between no-underline lg:col-span-4"
       >
-        <h2 className="border-0 py-4 text-muted text-xl capitalize">
-          {category} {posts.length > 0 && `(${posts.length})`}
-        </h2>
+        <hgroup>
+          <h2 className="-ml-[.06em] ~text-4xl/8xl border-0 text-iris-5 capitalize leading-none tracking-[-0.07em]">
+            {category}
+          </h2>
+          <p className="m-0 text-muted ~text-sm/base">試行の記録</p>
+        </hgroup>
       </NextViewTransition>
-
-      {posts.map((post) => {
-        return (
-          <React.Fragment key={post.slug}>
-            <Seperator />
-            <NextViewTransition
-              href={`/${category}/${post.slug}`}
-              className="flex w-full flex-col justify-between gap-1 py-4 leading-normal no-underline md:flex-row md:gap-16"
-            >
-              <p>{post.title}</p>
-              <p className="mt-0 shrink-0 text-muted">
-                {formatter.date(new Date(post.time.created))}
-              </p>
-            </NextViewTransition>
-          </React.Fragment>
-        );
-      })}
+      <div className="~gap-5/8 col-span-full grid lg:col-span-8">
+        {posts.map((post) => {
+          return (
+            <React.Fragment key={post.slug}>
+              <NextViewTransition
+                href={`/${category}/${post.slug}`}
+                className="gradient-card flex w-full flex-col justify-between gap-2 px-6 py-5 leading-normal no-underline [--border-color:var(--iris-4)] [--gradient-from:var(--iris-2)] [--gradient-to:var(--iris-1)] [--shadow-accent-color:var(--iris-a3)] [--shadow-base-color:var(--iris-a2)]"
+              >
+                <p className="font-bold text-xl">{post.title}</p>
+                <p className="mt-0 shrink-0 text-muted">
+                  {formatter.date(new Date(post.time.created))}
+                </p>
+              </NextViewTransition>
+            </React.Fragment>
+          );
+        })}
+      </div>
     </div>
   );
 };
