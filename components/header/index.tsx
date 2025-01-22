81473fb4e692ba94a6091e981619c34f21730201
osgsm <50955473+osgsm@users.noreply.github.com>
2025-01-22 15:47:14 +0900

    Update styles

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Header = () => {
   return (
     <header className="sticky top-0 z-50 grid w-full place-items-center bg-gradient-to-b from-5% from-[--iris-1] to-50% px-4 py-3 lg:my-6">
-      <div className="gradient-card flex w-full items-center justify-between gap-4 rounded-xl border border-iris-4 px-4 py-2 backdrop-blur-md [--gradient-from:var(--iris-a4)] [--gradient-to:var(--iris-a3)] [--shadow-accent-color:var(--iris-a4)] [--shadow-base-color:var(--iris-a2)] sm:w-fit dark:border-0 dark:backdrop-brightness-[0.2]">
+      <div className="gradient-card flex w-full items-center justify-between gap-4 rounded-xl border border-iris-4 px-4 py-3 backdrop-blur-md [--gradient-from:var(--iris-a4)] [--gradient-to:var(--iris-a3)] [--shadow-accent-color:var(--iris-a4)] [--shadow-base-color:var(--iris-a2)] sm:w-fit dark:border-0 dark:backdrop-brightness-[0.2]">
         <Link className="transition" href="/">
           <div
             className="size-7 animate-move-background bg-[length:600%] bg-gradient-to-br from-50% from-[--iris-8] via-[--iris-9] to-90% to-[--iris-10]"
@@ -29,7 +29,7 @@ const Header = () => {
           ].map(({ label, href }) => (
             <Link
               key={label}
-              className="rounded-lg ~text-sm/base px-2 py-0.5 transition hover:bg-[--iris-4] hover:text-white hover:opacity-100"
+              className="~text-sm/base rounded-lg px-2 py-0.5 transition hover:bg-[--iris-4] hover:text-white hover:opacity-100"
               href={href}
             >
               {label}
--- components/posts/index.tsx
+++ components/posts/index.tsx
@@ -25,34 +25,34 @@ export const Posts = ({ category, numberOfPosts }: PostProps) => {
 
   return (
     <>
-      <div className="mt-4 grid lg:grid-cols-12 gap-8 pb-8">
-        <NextViewTransition
-          href={`/${category}`}
-          className="col-span-full flex justify-between no-underline lg:col-span-4"
-        >
-          <hgroup>
+      <div className="mt-4 grid items-start gap-8 pb-8 lg:grid-cols-12">
+        <div className="sticky top-14 col-span-full flex justify-between no-underline lg:col-span-4">
+          <hgroup className="grid content-start gap-2">
             <h2 className="-ml-[.06em] ~text-5xl/8xl border-0 text-iris-7 capitalize leading-none tracking-[-0.07em]">
               {category}
             </h2>
-            <p className="~text-sm/base m-0 text-muted">
+            <p className="~text-sm/base m-0 font-bold text-iris-7">
               {category === "blog"
                 ? "試行の記録とか学習メモ"
                 : "ブログに満たないコードスニペットとか"}
             </p>
           </hgroup>
-        </NextViewTransition>
+        </div>
         <div className="~gap-5/8 col-span-full grid lg:col-span-8">
           {posts.slice(0, numberOfPosts).map((post) => {
             return (
               <React.Fragment key={post.slug}>
                 <NextViewTransition
                   href={`/${category}/${post.slug}`}
-                  className="gradient-card flex w-full flex-col justify-between gap-2 px-6 py-5 leading-normal no-underline border border-[--border-color] dark:border-0 [--border-color:var(--iris-4)] [--gradient-from:var(--iris-2)] [--gradient-to:var(--iris-1)] [--shadow-accent-color:var(--iris-a3)] [--shadow-base-color:var(--iris-a2)]"
+                  className="gradient-card relative flex w-full flex-col justify-between gap-2 border border-[--border-color] py-5 pr-16 pl-6 leading-normal no-underline [--border-color:var(--iris-4)] [--gradient-from:var(--iris-2)] [--gradient-to:var(--iris-1)] [--shadow-accent-color:var(--iris-a3)] [--shadow-base-color:var(--iris-a2)] dark:border-0"
                 >
                   <p className="~text-base/xl font-bold">{post.title}</p>
                   <p className="~text-sm/base mt-0 shrink-0 text-muted">
                     {formatter.date(new Date(post.time.created))}
                   </p>
+                  <div className="absolute right-4 bottom-4 grid size-8 place-items-center rounded-full border border-iris-4">
+                    <ArrowRightIcon className="text-muted" />
+                  </div>
                 </NextViewTransition>
               </React.Fragment>
             );
