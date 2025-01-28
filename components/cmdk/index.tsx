550a28fe049fba222e8e075d3496b9b9f36927a9
osgsm <50955473+osgsm@users.noreply.github.com>
2025-01-28 15:27:35 +0900

    Add button for cmdk

--- components/cmdk/index.tsx
+++ components/cmdk/index.tsx
@@ -8,7 +8,14 @@ import type { IconType } from "react-icons";
 import { cn } from "@/lib/cn";
 
 import { Command } from "cmdk";
-import { ArrowRight, BookText, FilePenLine, Moon, Sun } from "lucide-react";
+import {
+  ArrowRight,
+  BookText,
+  FilePenLine,
+  Moon,
+  Search,
+  Sun,
+} from "lucide-react";
 import { useTheme } from "next-themes";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -75,127 +82,139 @@ export const CommandMenu = ({
   }, []);
 
   return (
-    <Command.Dialog
-      open={open}
-      onOpenChange={setOpen}
-      label="コマンドパレットメニュー"
-      loop
-      contentClassName="fixed inset-0 z-50 p-8 md:p-[10vh] pointer-events-none backdrop-blur-md backdrop-brightness-50"
-      className={cn(
-        "pointer-events-auto relative mx-auto w-full max-w-[50rem] rounded-xl border border-iris-4 bg-iris-2 leading-snug",
-        "[&_[cmdk-group-heading]]:mt-2 [&_[cmdk-group-heading]]:mb-1 [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:text-muted",
-      )}
-    >
-      <div className="relative border-iris-4 border-b px-4 py-3">
-        <Command.Input
-          value={search}
-          onValueChange={setSearch}
-          placeholder="Type a command or search..."
-          className="bg-transparent outline-none placeholder:text-muted"
-        />
-        <button
-          type="button"
-          className="-translate-y-1/2 absolute top-1/2 right-3 rounded-md border border-iris-6 bg-iris-3 px-1 py-1 font-mono text-iris-10 text-sm leading-none"
-          onClick={() => setOpen(false)}
-        >
-          esc
-        </button>
-      </div>
-      <Command.List className="max-h-[calc(100vh-4rem-47px)] overflow-y-auto p-2 md:max-h-[calc(100vh-20vh-47px)]">
-        <Command.Empty className="p-2 text-iris-10">
-          No results found.
-        </Command.Empty>
-        <Command.Group heading="Blog">
-          {blogPosts.slice(0, search ? undefined : 3).map(({ slug, title }) => (
-            <CommandItem
-              key={slug}
-              label={title}
-              icon={BookText}
-              onSelect={() => {
-                setSearch("");
-                setOpen(false);
-                router.push(`/blog/${slug}`);
-              }}
-            />
-          ))}
-        </Command.Group>
-        <Command.Separator className="mt-2 border-border border-t py-1" />
-        <Command.Group heading="Notes">
-          {notesPosts
-            .slice(0, search ? undefined : 3)
-            .map(({ slug, title }) => (
+    <>
+      <button
+        type="button"
+        className="flex items-center gap-1 rounded-md bg-iris-5 py-1 pr-2 pl-1.5 text-iris-11 text-sm"
+        onClick={() => setOpen(true)}
+      >
+        <Search size={16} />
+        ⌘K
+      </button>
+      <Command.Dialog
+        open={open}
+        onOpenChange={setOpen}
+        label="コマンドパレットメニュー"
+        loop
+        contentClassName="fixed inset-0 z-50 p-8 md:p-[10vh] pointer-events-none backdrop-blur-md backdrop-brightness-50"
+        className={cn(
+          "pointer-events-auto relative mx-auto w-full max-w-[50rem] rounded-xl border border-iris-4 bg-iris-2 leading-snug",
+          "[&_[cmdk-group-heading]]:mt-2 [&_[cmdk-group-heading]]:mb-1 [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:text-muted",
+        )}
+      >
+        <div className="relative border-iris-4 border-b px-4 py-3">
+          <Command.Input
+            value={search}
+            onValueChange={setSearch}
+            placeholder="Type a command or search..."
+            className="bg-transparent outline-none placeholder:text-muted"
+          />
+          <button
+            type="button"
+            className="-translate-y-1/2 absolute top-1/2 right-3 rounded-md border border-iris-6 bg-iris-3 px-1 py-1 font-mono text-iris-10 text-sm leading-none"
+            onClick={() => setOpen(false)}
+          >
+            esc
+          </button>
+        </div>
+        <Command.List className="max-h-[calc(100vh-4rem-47px)] overflow-y-auto p-2 md:max-h-[calc(100vh-20vh-47px)]">
+          <Command.Empty className="p-2 text-iris-10">
+            No results found.
+          </Command.Empty>
+          <Command.Group heading="Blog">
+            {blogPosts
+              .slice(0, search ? undefined : 3)
+              .map(({ slug, title }) => (
+                <CommandItem
+                  key={slug}
+                  label={title}
+                  icon={BookText}
+                  onSelect={() => {
+                    setSearch("");
+                    setOpen(false);
+                    router.push(`/blog/${slug}`);
+                  }}
+                />
+              ))}
+          </Command.Group>
+          <Command.Separator className="mt-2 border-border border-t py-1" />
+          <Command.Group heading="Notes">
+            {notesPosts
+              .slice(0, search ? undefined : 3)
+              .map(({ slug, title }) => (
+                <CommandItem
+                  key={slug}
+                  label={title}
+                  icon={FilePenLine}
+                  onSelect={() => {
+                    resetSearch();
+                    router.push(`/notes/${slug}`);
+                  }}
+                />
+              ))}
+          </Command.Group>
+          <Command.Separator className="mt-2 border-border border-t py-1" />
+          <Command.Group heading="Navigation">
+            {[
+              { label: "Home", href: "/" },
+              { label: "Blog", href: "/blog" },
+              { label: "Notes", href: "/notes" },
+              { label: "About", href: "/about" },
+            ].map(({ label, href }) => (
               <CommandItem
-                key={slug}
-                label={title}
-                icon={FilePenLine}
+                key={href}
+                label={label}
+                icon={ArrowRight}
                 onSelect={() => {
                   resetSearch();
-                  router.push(`/notes/${slug}`);
+                  router.push(href);
                 }}
               />
             ))}
-        </Command.Group>
-        <Command.Separator className="mt-2 border-border border-t py-1" />
-        <Command.Group heading="Navigation">
-          {[
-            { label: "Home", href: "/" },
-            { label: "Blog", href: "/blog" },
-            { label: "Notes", href: "/notes" },
-            { label: "About", href: "/about" },
-          ].map(({ label, href }) => (
+          </Command.Group>
+          <Command.Separator className="mt-2 border-border border-t py-1" />
+          <Command.Group heading="Links">
+            {[
+              {
+                label: "Bluesky",
+                href: "https://bsky.app/profile/osgsm.io",
+                icon: FaBluesky,
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/osgsm",
+                icon: FaGithub,
+              },
+              {
+                label: "X (Twitter)",
+                href: "https://x.com/osgsm_",
+                icon: FaXTwitter,
+              },
+            ].map(({ label, href, icon: Icon }) => (
+              <CommandItem
+                key={href}
+                label={label}
+                icon={Icon}
+                onSelect={() => {
+                  resetSearch();
+                  window.open(href, "_blank");
+                }}
+              />
+            ))}
+          </Command.Group>
+          <Command.Separator className="mt-2 border-border border-t py-1" />
+          <Command.Group heading="Command">
             <CommandItem
-              key={href}
-              label={label}
-              icon={ArrowRight}
+              label={`Toggle theme (change to ${theme === "light" ? "dark" : "light"} mode)`}
+              icon={theme === "light" ? Moon : Sun}
               onSelect={() => {
                 resetSearch();
-                router.push(href);
+                setTheme(theme === "light" ? "dark" : "light");
               }}
             />
-          ))}
-        </Command.Group>
-        <Command.Separator className="mt-2 border-border border-t py-1" />
-        <Command.Group heading="Links">
-          {[
-            {
-              label: "Bluesky",
-              href: "https://bsky.app/profile/osgsm.io",
-              icon: FaBluesky,
-            },
-            {
-              label: "GitHub",
-              href: "https://github.com/osgsm",
-              icon: FaGithub,
-            },
-            {
-              label: "X (Twitter)",
-              href: "https://x.com/osgsm_",
-              icon: FaXTwitter,
-            },
-          ].map(({ label, href, icon: Icon }) => (
-            <CommandItem
-              key={href}
-              label={label}
-              icon={Icon}
-              onSelect={() => {
-                resetSearch();
-                window.open(href, "_blank");
-              }}
-            />
-          ))}
-        </Command.Group>
-        <Command.Separator className="mt-2 border-border border-t py-1" />
-        <Command.Group heading="Command">
-          <CommandItem
-            label={`Toggle theme (change to ${theme === "light" ? "dark" : "light"} mode)`}
-            icon={theme === "light" ? Moon : Sun}
-            onSelect={() => {
-              resetSearch();
-              setTheme(theme === "light" ? "dark" : "light");
-            }}
-          />
-        </Command.Group>
-      </Command.List>
-    </Command.Dialog>
+          </Command.Group>
+        </Command.List>
+      </Command.Dialog>
+    </>
   );
 };
--- components/header/index.tsx
+++ components/header/index.tsx
@@ -17,7 +17,7 @@ const Header = () => {
     <header className="sticky top-0 z-40 grid w-full place-items-center from-5% from-[--iris-1] to-50% px-4 py-3 lg:my-6 dark:bg-gradient-to-b">
       <div
         className={cn(
-          "gradient-card flex w-full items-center justify-between gap-4 rounded-xl border border-iris-4 px-4 py-3 backdrop-blur-md",
+          "gradient-card flex w-full items-center justify-between gap-2 rounded-xl border border-iris-4 px-3 py-3 backdrop-blur-md md:gap-4 md:px-4",
           "sm:w-fit",
           "[--gradient-from:var(--white-a11)] [--gradient-to:var(--white-a9)] [--shadow-accent-color:var(--white-a12)] [--shadow-base-color:var(--iris-a5)]",
           "dark:border-0 dark:backdrop-brightness-[0.2] dark:[--gradient-from:var(--iris-a4)] dark:[--gradient-to:var(--iris-a3)] dark:[--shadow-accent-color:var(--iris-a4)] dark:[--shadow-base-color:var(--iris-a2)]",
@@ -48,15 +48,15 @@ const Header = () => {
           ].map(({ label, href }) => (
             <Link
               key={label}
-              className="~text-sm/base rounded-lg px-2 py-0.5 transition hover:bg-[--iris-4] hover:text-white hover:opacity-100"
+              className="~text-sm/base rounded-md px-1.5 py-0.5 transition hover:bg-[--iris-4] hover:text-white hover:opacity-100 md:px-2"
               href={href}
             >
               {label}
             </Link>
           ))}
         </div>
+        <CommandMenu blogPosts={blogPosts} notesPosts={notesPosts} />
       </div>
-      <CommandMenu blogPosts={blogPosts} notesPosts={notesPosts} />
     </header>
   );
 };
