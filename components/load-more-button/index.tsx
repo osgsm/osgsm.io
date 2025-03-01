d44de2ae66a74ed502fbce32506cb584b9eadf82
osgsm <50955473+osgsm@users.noreply.github.com>
2025-03-01 15:35:31 +0900

    Fix font size of LoadMoreButton component

--- components/load-more-button/index.tsx
+++ components/load-more-button/index.tsx
@@ -14,7 +14,7 @@ export const LoadMoreButton = ({
     <motion.button
       type="button"
       onClick={onClick}
-      className="flex items-center gap-2 rounded-md border border-iris-6 px-4 py-2.5 font-medium text-iris-9 transition-colors hover:border-iris-7 hover:text-iris-10"
+      className="flex items-center gap-2 rounded-md border border-iris-6 px-4 py-2.5 font-medium text-iris-9 text-sm transition-colors hover:border-iris-7 hover:text-iris-10"
       whileHover="hover"
     >
       {label}
